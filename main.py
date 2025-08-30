e5f81d9f6708669411764669b1ee6e94dd287db1
tarncairn <193822639+tarncairn@users.noreply.github.com>
2025-08-30 00:31:19 -0600

    get num words

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def get_book_text(filepath):
         contents = f.read()
     return contents
 
+def get_num_words(contents):
+    num_words = len(contents.split())
+    return num_words
 
 def main():
     filepath = "books/frankenstein.txt"
     contents = get_book_text(filepath)
-   
+    num_words = get_num_words
+    print(f"{num_words} words found in the document")
 
 main()
