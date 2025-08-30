9b5ffb6bf4f6b3441b06489458b2d59f8f4ad6dd
tarncairn <193822639+tarncairn@users.noreply.github.com>
2025-08-30 00:35:30 -0600

    count character instances

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, count_character_instances
 
 def get_book_text(filepath):
     contents = ""
@@ -12,5 +12,6 @@ def main():
     contents = get_book_text(filepath)
     num_words = get_num_words
     print(f"{num_words} words found in the document")
-
+    character_instances = count_character_instances(contents)
+    print(character_instances)
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_num_words(contents):
     num_words = len(contents.split())
     return num_words
+
+def count_character_instances(contents):
+    contents = contents.lower().split()
+    count = {}
+    for word in contents:
+        for char in word:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+    
+    return count
